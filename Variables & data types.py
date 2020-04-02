b4e2a8b1b94dc9312ff635ec05a5776936212221
Omar Monrroy <ogcm.kael@hotmail.com>
2020-04-02 17:50:41 -0600

    Variables & data types

---
+++ Variables & data types.py
@@ -0,0 +1,8 @@
+characters_name = "Omar"
+characters_age = "20"
+isMale = True
+
+print("There once was a man named " + characters_name + " ,")
+print("He was " + characters_age + " years old. ")
+print("He really liked the name " + characters_name + ", ")
+print("but didn't like being " + characters_age + ".")
