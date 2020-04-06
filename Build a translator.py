d5bf2cdbf6766f2fbf997544e2af624469fc7958
Omar Monrroy <ogcm.kael@hotmail.com>
2020-04-05 21:31:22 -0500

    Upgrating the translator

--- Build a translator.py
+++ Build a translator.py
@@ -1,7 +1,7 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter == 'a' or letter == 'e'or letter == 'i' or letter == 'o' or letter == 'u':
+        if letter in 'AEIOUaeiou':
             translation = translation + 'g'
         else:
             translation = translation + letter
